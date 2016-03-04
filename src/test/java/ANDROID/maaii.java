f8c1bb493953f258c6a9ea7a2de31a5e92d27a07
Anson Liao <ansonliao.xiao@gmail.com>
2016-03-04 18:51:46 +0800

    Add swipe for Android test case.

--- src/test/java/ANDROID/maaii.java
+++ src/test/java/ANDROID/maaii.java
@@ -6,7 +6,10 @@ import com.maaii.automation.exception.NoSuchLocatorException;
 import com.maaii.automation.exception.NoSuchLocatorExistException;
 import com.maaii.automation.page.Action;
 import com.maaii.automation.testcase.AndroidBaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Set;
 
 /**
  * Created by ansonliao on 4/3/2016.
@@ -15,6 +18,26 @@ public class maaii extends AndroidBaseTest {
 
     @Test
     public void f1() throws IllegalLocatorIndexException, NoSuchLocatorExistException, LocatorDisplayException, NoSuchLocatorException, InterruptedException {
-        Action.clickAndWait(page.getElement("termsBtn"), 10000);
+        Action.click(page.getElement("termsBtn"));
+        Assert.assertEquals("Terms of Service", page.getElement("termsTitle").getElement().getText(), "Terms page's title incorrect.");
+//        System.out.println(page.getElementNoWait("termsTitle").getElement().getText());
+
+//        Set<String> contextNames = driver.getContextHandles();
+//        for (String contextName : contextNames) {
+//            System.out.println("ContextName: " + contextName);
+//            if (contextName.contains("WEBVIEW")) {
+//                driver.context(contextName);
+//            }
+//        }
+//
+//        System.out.println("Terms content title: " + driver.findElementById("maaii-terms-of-service").getText());
+        Thread.sleep(10000);
+        System.out.println("Start scroll...");
+        int width = driver.manage().window().getSize().width;
+        int heigh = driver.manage().window().getSize().height;
+
+        driver.swipe(width/2, heigh/10*8, width/2, heigh/10*2, 1000);
+
+        Thread.sleep(5000);
     }
 }
